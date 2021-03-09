c688ce18373476b7ff8b371b29c32fa0e8de9731
Gabriel <gbsm1993@gmail.com>
2021-03-08 21:59:25 -0300

    adds more robust test for selection

--- Intelligens.Tests/Optimization.GeneticAlgorithm/TournamentSelectionStrategyTest.cs
+++ Intelligens.Tests/Optimization.GeneticAlgorithm/TournamentSelectionStrategyTest.cs
@@ -7,6 +7,42 @@ namespace Intelligens.Tests.Optimization.GeneticAlgorithm
     public class TournamentSelectionStrategyTest
     {
         [Fact]
+        public void Selection_ShouldPreferFitterOverTime()
+        {
+            // Arrange
+            var prng = new DefaultRandomGenerator();
+            var selectionStrategy = new TournamentSelectionStrategy<int>(5, prng);
+            var population = new []
+            {
+                new [] {9, 9, 9},
+                new [] {8, 8, 8},
+                new [] {7, 7, 7},
+                new [] {6, 6, 6},
+                new [] {5, 5, 5},
+                new [] {4, 4, 4},
+                new [] {3, 3, 3},
+                new [] {2, 2, 2},
+                new [] {1, 1, 1},
+                new [] {0, 0, 0},
+            };
+
+            var count = 10000;
+            var sum = 0.0;
+
+            // Act
+            for (var i = 0; i < count; i += 1)
+            {
+                var pair = selectionStrategy.Select(population, null);
+                sum += (pair.Item1[0] + pair.Item2[0]) / 2.0;
+            }
+
+            var average = sum / count;
+
+            // Assert
+            Assert.True(average > 5);
+        }
+
+        [Fact]
         public void Tournament_ShouldSelectTheBest()
         {
             // Arrange
