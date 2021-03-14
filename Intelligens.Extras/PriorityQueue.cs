5e4aec31d12f3839994601bd175a357e79d40f69
Gabriel <gbsm1993@gmail.com>
2021-03-14 01:03:18 -0300

    priority queue can be simpler and still good

--- Intelligens.Extras/PriorityQueue.cs
+++ Intelligens.Extras/PriorityQueue.cs
@@ -5,7 +5,6 @@ namespace Intelligens.Extras
 {
     public class PriorityQueue<T, U> where U : IComparable
     {
-        private readonly bool _greaterFirst;
         private readonly int _capacity;
         private readonly List<T> _items;
         private readonly List<U> _priorities;
@@ -18,16 +17,12 @@ namespace Intelligens.Extras
             }
         }
 
-        public PriorityQueue(int capacity, bool greaterFirst)
+        public PriorityQueue(int capacity)
         {
             _capacity = capacity;
-            _greaterFirst = greaterFirst;
             _items = new List<T>(_capacity);
             _priorities = new List<U>(_capacity);
         }
-
-        public PriorityQueue(int capacity) : this(capacity, true)
-        { }
 
         public void Add(T item, U priority)
         {
@@ -35,7 +30,7 @@ namespace Intelligens.Extras
 
             for (i = 0; i < _priorities.Count; i += 1)
             {
-                if (FoundPosition(priority, _priorities[i]))
+                if (priority.CompareTo(_priorities[i]) > 0)
                 {
                     break;
                 }
@@ -60,11 +55,6 @@ namespace Intelligens.Extras
                     _items.RemoveAt(_capacity);
                 }
             }
-        }
-
-        private bool FoundPosition(U item1, U item2)
-        {
-            return _greaterFirst ? item1.CompareTo(item2) > 0 : item1.CompareTo(item2) < 0;
         }
     }
 }
--- Intelligens.Tests/Extras/PriorityQueueTest.cs
+++ Intelligens.Tests/Extras/PriorityQueueTest.cs
@@ -56,24 +56,5 @@ namespace Intelligens.Test.Extras
             // Assert
             Assert.Equal(expected, actual);
         }
-
-        [Fact]
-        public void Add_ShouldInvertOrder_WithLesserFirst()
-        {
-            // Arrange
-            var q = new PriorityQueue<string, int>(3, false);
-            var expected = new [] {"item1", "item2", "item3"};
-
-            // Act
-            q.Add("item2", 2);
-            q.Add("item1", 1);
-            q.Add("item5", 5);
-            q.Add("item4", 4);
-            q.Add("item3", 3);
-            var actual = q.Items;
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
     }
 }
