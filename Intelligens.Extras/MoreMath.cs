3fbae5ab9efc6fa33358104587589aeb2d00d5fb
Gabriel <gbsm1993@gmail.com>
2021-03-19 21:36:49 -0300

    add matrix multiplication wow

--- Intelligens.Extras/MoreMath.cs
+++ Intelligens.Extras/MoreMath.cs
@@ -16,5 +16,31 @@ namespace Intelligens.Extras
 
             return Math.Sqrt(acc);
         }
+
+        public static double[,] Multiply(double[,] a, double[,] b)
+        {
+            var rowCount = a.GetLength(0);
+            var colCount = b.GetLength(1);
+            var m = a.GetLength(1);
+
+            var r = new double[rowCount, colCount];
+
+            for (var row = 0; row < rowCount; row += 1)
+            {
+                for (var col = 0; col < colCount; col += 1)
+                {
+                    var sum = 0.0;
+
+                    for (var k = 0; k < m; k += 1)
+                    {
+                        sum += a[row, k] * b[k, col];
+                    }
+
+                    r[row, col] = sum;
+                }
+            }
+
+            return r;
+        }
     }
 }
---
+++ Intelligens.Tests/Extras/MoreMathTest.cs
@@ -0,0 +1,94 @@
+using Intelligens.Extras;
+using Xunit;
+
+namespace Intelligens.Tests.Extras
+{
+    public class MoreMathTest
+    {
+        [Fact]
+        public void Multiply_ShouldWork_WithSquareMatrices1()
+        {
+            // Arrange
+            var a = new double[,]
+            {
+                {0, 1},
+                {0, 0}
+            };
+
+            var b = new double[,]
+            {
+                {0, 0},
+                {1, 0},
+            };
+
+            var expected = new double[,]
+            {
+                {1, 0},
+                {0, 0}
+            };
+
+            // Act
+            var actual = MoreMath.Multiply(a, b);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Multiply_ShouldWork_WithSquareMatrices2()
+        {
+            // Arrange
+            var a = new double[,]
+            {
+                {0, 0},
+                {1, 0}
+            };
+
+            var b = new double[,]
+            {
+                {0, 1},
+                {0, 0},
+            };
+
+            var expected = new double[,]
+            {
+                {0, 0},
+                {0, 1}
+            };
+
+            // Act
+            var actual = MoreMath.Multiply(a, b);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Multiply_ShouldWork_WithDifferentMatrices()
+        {
+            // Arrange
+            var a = new double[,]
+            {
+                {3, 4, 2},
+            };
+
+            var b = new double[,]
+            {
+                {13, 9, 7, 15},
+                {8, 7, 4, 6},
+                {6, 4, 0, 3}
+            };
+
+            var expected = new double[,]
+            {
+                {83, 63, 37, 75},
+            };
+
+            // Act
+            var actual = MoreMath.Multiply(a, b);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
